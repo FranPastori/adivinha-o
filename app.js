ddd245a5d568426e83968ec7acf7072e753c949b
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2024-04-21 22:40:00 +0000

    xcxc

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ let chute;
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
-if (chute == numeroSecreto) { 
+    if (chute == numeroSecreto) {
         alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
+    } else {
+        if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
-} else {
-        alert(`0 número secreto é maior que ${chute}`)
-}
-}
+        } else {
+            alert(`0 número secreto é maior que ${chute}`)
+        }
+    }
 }
