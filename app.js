eb84e2bc074b76e6bf5f080a7972017ab0248e1f
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2024-04-21 22:51:04 +0000

    xsx

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let numeroSecreto = 29;
 
 let chute;
+let tentativas;
 
 /*if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
@@ -16,12 +17,20 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        break;
+       
+    
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`0 número secreto é maior que ${chute}`)
         }
+        tentativas++
     }
+}
+if (tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
 }
