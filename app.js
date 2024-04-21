504a57a86b3e3a4e3d3e0de876ee97b1ccb2cb6a
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2024-04-21 22:37:07 +0000

    knk

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 let numeroSecteto = 29;
-prompt('escreva um número entre 1 e 30');
+
+let chute = prompt('escreva um número entre 1 e 30');
+
+/*if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
+}
+*/
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+if (chute == numeroSecreto) { 
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+} else {
+        alert(`0 número secreto é maior que ${chute}`)
+}
